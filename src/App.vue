05a172d5f16c6b889d11498f66e937eb518344f8
0x0u <m0zurillex@gmail.com>
2020-12-04 20:21:47 +0900

    :wrench: vue-router関連のセットアップ

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,19 @@
   <v-app id="app">
     <v-main>
       <Header/>
-      <Contents/>
+      <router-view/>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header'
-import Contents from './components/Contents'
 
 export default {
   name: 'App',
 
   components: {
     Header,
-    Contents
   },
 }
 </script>
