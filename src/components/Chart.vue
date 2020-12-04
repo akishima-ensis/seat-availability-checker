9be3d4319adbab8aa091bd3bd59fc231cfa04370
0x0u <m0zurillex@gmail.com>
2020-12-04 15:20:31 +0900

    :wrench: チャート描画コンポーネントの追加

---
+++ src/components/Chart.vue
@@ -0,0 +1,21 @@
+<script>
+import { Bar } from 'vue-chartjs'
+
+export default {
+  extends: Bar,
+  props: {
+    chartData: {
+      type: Object,
+      default: null
+    },
+    options: {
+      type: Object,
+      default: null,
+      responsive: false
+    },
+  },
+  mounted () {
+    this.renderChart(this.chartData, this.options)
+  }
+}
+</script>
