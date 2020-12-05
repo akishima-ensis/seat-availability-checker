fc4d099e7efda26383206e6007082634caeba794
0x0u <m0zurillex@gmail.com>
2020-12-06 02:48:24 +0900

    :sparkles: ダークモードと通常モードを切り替えるスイッチの追加

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-  <v-app-bar dense flat @click="jumpTop">アキシマエンシス空席チェッカー</v-app-bar>
+  <v-app-bar dense flat>
+    <v-toolbar-title>アキシマエンシス空席チェッカー</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <div>
+      <v-switch v-model="darkMode" :prepend-icon="themeIcon" hide-details label=""></v-switch>
+    </div>
+  </v-app-bar>
 </template>
 
 <script>
@@ -7,6 +13,21 @@ import router from '@/router'
 
 export default {
   name: 'Header',
+  data() {
+    return {
+      darkMode: true
+    }
+  },
+  computed: {
+    themeIcon() {
+      return this.darkMode ? 'mdi-weather-night' : 'mdi-weather-sunny'
+    }
+  },
+  watch: {
+    darkMode() {
+      this.$vuetify.theme.dark = this.darkMode
+    }
+  },
   methods: {
     jumpTop() {
       if (this.$route.path === '/about') {
