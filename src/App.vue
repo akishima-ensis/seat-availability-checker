1105b21a57540f3c97f1f45f7f7a1d97791730c9
0x0u <m0zurillex@gmail.com>
2020-12-14 19:16:44 +0900

    :wrench: フッターの追加

--- src/App.vue
+++ src/App.vue
@@ -3,18 +3,21 @@
     <v-main>
       <Header/>
       <router-view/>
+      <Footer></Footer>
     </v-main>
   </v-app>
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default {
   name: 'App',
 
   components: {
     Header,
+    Footer
   },
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-footer padless>
+    <v-col class="text-center" cols="12">
+      © {{ new Date().getFullYear() }} - ensis-seat-checker
+    </v-col>
+  </v-footer>
+</template>
